6ba60bb95969ba9da83fb316ac2655b63e2ad18a
Verna <yuhsichen0214@gmail.com>
2023-12-26 19:08:00 +0800

    feat: project init

---
+++ app.js
@@ -0,0 +1,14 @@
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
+const express = require('express')
+
+const routes = require('./routes')
+const app = express()
+const port = process.env.PORT
+
+app.use(routes)
+
+app.listen(port, () => {
+  console.info(`App is running on http://localhost:${port}.`)
+})
