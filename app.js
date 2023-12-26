61bb8dc111f788113b5812d233421626240590cb
Verna <yuhsichen0214@gmail.com>
2023-12-26 19:32:07 +0800

    feat: sequelize init & connect

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const express = require('express')
 
 const routes = require('./routes')
 const app = express()
-const port = process.env.PORT
+const port = process.env.PORT || 3000
 
 app.use(routes)
 
