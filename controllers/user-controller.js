bd1de8a4f5c8963c5ea5b6ce63ff0d405c56ce1d
Verna <yuhsichen0214@gmail.com>
2024-01-09 10:57:41 +0800

    modify: response msg

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -7,11 +7,11 @@ const userController = {
     try {
       const { name, nickName, email, password, tel, lineId } = req.body
       if (!name || !email || !password || !tel) {
-        return res.status(400).json({ status: '請求錯誤', error: '名字、信箱、密碼、電話欄位不得空白！' })
+        return res.status(400).json({ status: 'error', error: '名字、信箱、密碼、電話欄位不得空白！' })
       }
       let user = await User.findOne({ where: { email } })
       if (user) {
-        return res.status(400).json({ status: '請求錯誤', error: '使用者已註冊！' })
+        return res.status(400).json({ status: 'error', error: '使用者已註冊！' })
       }
       user = await User.create({
         name,
@@ -23,7 +23,7 @@ const userController = {
       })
       return res.status(200).json({ status: 'success', user })
     } catch (err) {
-      return res.status(500).json({ error: '伺服器內部錯誤', err })
+      return res.status(500).json({ error: 'error', err })
     }
   }
 }
--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ const router = express.Router()
 
 const userController = require('../controllers/user-controller')
 
-router.post('/signup', userController.signUp)
+router.post('/api/users/signup', userController.signUp)
 router.get('/', (req, res) => res.send('Hello, world!'))
 
 module.exports = router
