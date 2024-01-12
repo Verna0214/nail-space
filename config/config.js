6965bcc7f1c20440987ec450e57a033764000f52
Verna <yuhsichen0214@gmail.com>
2024-01-12 10:54:42 +0800

    feat: add eb migration.config

--- config/config.js
+++ config/config.js
@@ -17,10 +17,11 @@ module.exports = {
     dialect: "mysql"
   },
   production: {
-    username: "root",
-    password: null,
-    database: "database_production",
-    host: "127.0.0.1",
-    dialect: "mysql"
+    host: process.env.RDS_HOSTNAME,
+    username: process.env.RDS_USERNAME,
+    password: process.env.RDS_PASSWORD,
+    port: process.env.RDS_PORT,
+    database: process.env.RDS_DB_NAME,
+    dialect: 'mysql'
   }
 }
